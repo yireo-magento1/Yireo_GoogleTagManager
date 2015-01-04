c08247f487e9317c78c931ddd009cf615f292058
Jisse Reitsma <info@yireo.com>
2015-01-04 09:46:41 +0100

    Pending changes

--- source/app/code/community/Yireo/GoogleTagManager/Block/Default.php
+++ source/app/code/community/Yireo/GoogleTagManager/Block/Default.php
@@ -4,7 +4,7 @@
  *
  * @package     Yireo_GoogleTagManager
  * @author      Yireo (http://www.yireo.com/)
- * @copyright   Copyright (C) 2014 Yireo (http://www.yireo.com/)
+ * @copyright   Copyright 2015 Yireo (http://www.yireo.com/)
  * @license     Open Source License (OSL v3)
  */
 
